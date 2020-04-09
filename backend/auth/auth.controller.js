e01fbf9ff4576e63204b8c38dc5c89ff8024aaae
Shaheer Shahzad <shaheer19962012@gmail.com>
2020-04-09 09:08:01 +0200

    Login/Register controller done.

--- backend/auth/auth.controller.js
+++ backend/auth/auth.controller.js
@@ -10,5 +10,58 @@ exports.createUser = (req, res, next) => {
         password: req.body.password
     }
 
-    User.create
+    User.create(newUser, (err, user) => {
+        if(err){
+            return res.status(500).send("Server error");
+        }
+
+        const expiresIn = 24*60*60;
+        const accessToken = jwt.sign({
+            id: user.id
+        }, SECRET_KEY, {
+            expiresIn: expiresIn
+        });
+
+        // response to frontend
+        Response.send({user});
+    });
+}
+
+exports.loginUser = (req, res, next) => {
+    const userData = {
+        email: req.body.email,
+        password: req. body.password
+    }
+
+    User.findOne({
+        email: userData.email
+    }, (err, user) => {
+        if(err){
+            return res.status(500).send("Server error");
+        }
+
+        if(!user){
+            // Email doesn't exist
+            res.status(409).send({
+                message: "Something is wrong"
+            });
+        }else{
+            const resultPassword = userData.password;
+            if(resultPassword){
+                const expiresIn = 24*60*60;
+                const accessToken = jwt.sign({
+                    id: user.id
+                }, SECRET_KEY, {
+                    expiresIn: expiresIn
+                });
+
+                res.send({ userData });
+            }else{
+                // Wrong password
+                res.status(409).send({
+                    message: "Something is wrong"
+                });
+            }
+        }
+    });
 }
